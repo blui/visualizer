b27818a7970e19bd0bcc6796193b0e308a626a17
unknown <bcllui@gmail.com>
2013-10-29 13:19:10 -0700

    - Cleaned up the readdir Method as well as the outputs for the file reader - Separated testing console output and actual browser output

--- app.js
+++ app.js
@@ -5,10 +5,37 @@
 
 var express = require('express');
 var app = express();
-var wrench = require('wrench'), util = require('util');
+var wrench = require('wrench'), util = require('util'), fs = require('fs');
 
+/**
+ * Function call using file system to traverse through files, reading lines
+ */
+
+var dir='./views/';	// Declare the directory to be scanned
+var data = {}	// Create array object for storage 
+ 
+fs.readdir(dir, function(err, files) {
+	if (err) {
+		throw err;
+	}
+	var c = 0;	// Declare a var c = 0; initial condition of a for loop
+	files.forEach(function(file) {
+		c++;	// Increment a counter in the for-loop condition
+		fs.readFile(dir+file, 'utf-8', function(err, string) {
+			if (err) {
+				throw err;
+			}
+			data[file] = string;	// Throws into data object string of contents within the file being read
+			//console.log(data);	// We only need this to test using console (the contents being stored)
+		});
+	});
+});	
+
+/*
+ * Function from express framework to pump out contents stored in 'data'
+ */
 app.get('/', function(req, res){
-	res.send('Hello World');
+	res.send(data);
 });
 
 // wrench.readdirSyncRecursive('routes');
@@ -32,26 +59,6 @@ app.get('/', function(req, res){
 			// util.puts(f.getNextLine());
 		// }
 	// }
-	
-// var fs = require('fs');
-
-// var dir='./routes/';
-// var data = {}
-
-// fs.readdir(dir, function(err, files) {
-		// if (err) throw err;
-		// var c = 0;
-		// files.forEach(function(file) {
-			// c++;
-			// fs.readFile(dir+file, 'utf-8', function(err, string) {
-				// if (err) throw err;
-				// data[file] = string;
-				// if (0 === -- c) {
-					// console.log(data);
-				// }
-			// });
-		// });
-// });		
 				
 app.listen(3000);
 console.log('Listening on port 3000');
