1bcbe02d70d313a151a91fbffd955da57cba7e2c
unknown <bcllui912@gmail.com>
2013-10-30 10:20:18 -0700

    Giant list of changes: - Added app.js to be able to run the entire application - Modified inputReader.js to become a simple module with functions - Integrating app.js with module dependency inputReader.js - Some errors persists; in progress of fixing them

--- inputReader.js
+++ inputReader.js
@@ -2,51 +2,38 @@
 /**
  * Class: inputReader.js (formally app.js)
  * Authors: Brian Lui, Miki Gataric
- * Description: Assigns directory manually and reads all files under directory. Prints out results on localhost.
+ * Description: Assigns directory manually and reads all files under directory and stores results
  */
 
-
-/**
+ /**
  * Module dependencies.
  */
-
-var express = require('express');
-var app = express();
-var wrench = require('wrench'), util = require('util'), fs = require('fs');
+var fs = require('fs');
 
 /**
  * Function call using file system to traverse through files, reading lines
  */
-
-var dir='./views/';	// Declare the directory to be scanned
-var data = {}	// Create array object for storage 
- 
-fs.readdir(dir, function(err, files) {
-	if (err) {
-		throw err;
-	}
-	var c = 0;	// Declare a var c = 0; initial condition of a for loop
-	files.forEach(function(file) {
-		c++;	// Increment a counter in the for-loop condition
-		fs.readFile(dir+file, 'utf-8', function(err, string) {
+module.exports = {
+	readInputWithDir: function() {
+		var dir = './views/';	// Declare the directory to be scanned
+		var data = {}	// Create array object for storage 
+		fs.readdir(dir, function(err, files) {
 			if (err) {
 				throw err;
 			}
-			data[file] = string;	// Throws into data object string of contents within the file being read
-			//console.log(data);	// We only need this to test using console (the contents being stored)
-		});
-	});
-});	
-
-/*
- * Function from express framework to pump out contents stored in 'data'
- */
-app.get('/', function(req, res){
-	res.send(data);
-});
-
-/*
- * Call to create a server on local host
- */
-app.listen(3000);
-console.log('Listening on port 3000');
+			var c = 0;	// Declare a var c = 0; initial condition of a for loop
+			files.forEach(function(file) {
+				c++;	// Increment a counter in the for-loop condition
+				fs.readFile(dir+file, 'utf-8', function(err, string) {
+					if (err) {
+						throw err;
+					}
+					if ( 0 === -3) {
+						data[file] = string;	// Throws into data object string of contents within the file being read
+						console.log(data);	// We only need this to test using console (the contents being stored)
+					}
+				});
+			});
+		});	
+	}
+};
