2c02caf537b3be30866a81e4953c97083a8a8cbd
Miki Gataric <m.gata1990@gmail.com>
2013-10-31 15:12:23 -0700

    implemented searching based on file extensions

--- directoryScanner.js
+++ directoryScanner.js
@@ -1,29 +1,28 @@
 var walk    = require('walk'), filter;
 var files   = [];
-var matches = 0;
 var i = 0;
-var filename = 'view.js';
+var filematches = 0;
+var fileextension = '.html';
 var foldername = 'node_modules';
 var filter = {
     followLinks: false, 
   };
-
 var walker  = walk.walkSync(foldername, filter);
 
 walker.on('file', function(root, stat, next) {
 	files.push(stat.name);
-	if (files[i].substring() == "js")
+	if (files[i].indexOf(fileextension) != -1)
 	{
 		console.log(files[i]);
 		next();
 		i++;
-		matches++;
+		filesmatches++;
 	}
 	else
 	{
 		next();
 		i++;
 	}
-	console.log('Number of matching files: ' + matches + " and number of items in the array: " + files.length)
+	console.log('Number of matching files: ' + filematches + " and number of items in the array: " + files.length)
 	return files;
 });
