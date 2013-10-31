229216da95f4d9e75c0c5c2236dcde20fcfb8d2a
Miki Gataric <m.gata1990@gmail.com>
2013-10-31 14:31:20 -0700

    made more improvements to code

--- directoryScanner.js
+++ directoryScanner.js
@@ -1,28 +1,29 @@
 var walk    = require('walk'), filter;
 var files   = [];
-var counter = 0;
+var matches = 0;
 var i = 0;
-
+var filename = 'view.js';
+var foldername = 'node_modules';
 var filter = {
     followLinks: false, 
-	filters: ['lib']
   };
 
-var walker  = walk.walkSync('node_modules', filter);
+var walker  = walk.walkSync(foldername, filter);
 
 walker.on('file', function(root, stat, next) {
 	files.push(stat.name);
-	if (files[i] == 'view.js')
+	if (files[i].substring() == "js")
 	{
 		console.log(files[i]);
 		next();
 		i++;
-		counter++;
+		matches++;
 	}
 	else
 	{
 		next();
 		i++;
 	}
-	console.log('Number of matching files: ' + counter + " and number of items in the array: " + files.length)
+	console.log('Number of matching files: ' + matches + " and number of items in the array: " + files.length)
+	return files;
 });
