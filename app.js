56d60d138caff58d92efd06312ea9cf6bdca8304
Brian <bcllui912@gmail.com>
2013-10-30 17:20:26 -0700

    File reader works; module exports work. :D File reader now runs from app.js

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
-
-/**
- * Class: app.js
- * Authors: Brian Lui, Miki Gataric
- * Description: Serves as the main class to run; should have dependencies linked between all classes
- */
  
 /**
- * Module dependencies.
+ * API dependencies
  */
 var express = require('express');
 var app = express();
-var wrench = require('wrench'), util = require('util'), fs = require('fs');
+fs = require('fs');
 
 /**
- * Declare dependency with other classes
+ * Module dependencies
  */
 var inputReader = require('./inputReader.js');
-
-module.exports = {
-	performTest: function() {
-		consolelog(inputReader.readInputWithDir());
-	}
-}
+var data = inputReader.contentsOfFile();
+console.log(data.toString());
 
 /*
  * Set up localhost with port 3000
  */
-app.listen(3000);
-console.log('Listening on port 3000');	//Notify on runtime which port is used
+//app.listen(3000);
+//console.log('Listening on port 3000');	//Notify on runtime which port is used
--- inputReader.js
+++ inputReader.js
@@ -1,39 +1,12 @@
 
-/**
- * Class: inputReader.js (formally app.js)
- * Authors: Brian Lui, Miki Gataric
- * Description: Assigns directory manually and reads all files under directory and stores results
- */
-
  /**
  * Module dependencies.
  */
 var fs = require('fs');
 
-/**
- * Function call using file system to traverse through files, reading lines
- */
 module.exports = {
-	readInputWithDir: function() {
-		var dir = './views/';	// Declare the directory to be scanned
-		var data = {}	// Create array object for storage 
-		fs.readdir(dir, function(err, files) {
-			if (err) {
-				throw err;
-			}
-			var c = 0;	// Declare a var c = 0; initial condition of a for loop
-			files.forEach(function(file) {
-				c++;	// Increment a counter in the for-loop condition
-				fs.readFile(dir+file, 'utf-8', function(err, string) {
-					if (err) {
-						throw err;
-					}
-					if ( 0 === -3) {
-						data[file] = string;	// Throws into data object string of contents within the file being read
-						console.log(data);	// We only need this to test using console (the contents being stored)
-					}
-				});
-			});
-		});	
+	contentsOfFile: function() {
+		var text = fs.readFileSync('./simpleParser.js', 'utf8');
+		return text;
 	}
 };
