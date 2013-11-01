321f6a68679c210f3f99f3feec33362f29ba589c
Miki Gataric <m.gata1990@gmail.com>
2013-10-31 20:54:16 -0700

    revised directory scanner to not use walker anymore

--- directoryScanner.js
+++ directoryScanner.js
@@ -3,42 +3,28 @@
 	Module: directoryScanner.js
 	Description: Basic directory reader returns string of contents of a directory from a set directory name
 	Needs to be done: Add key listener to allow for a dynamic directory name
-					  Add filters to be able to scan directorys selectively; ex. dir #1, dir #3 but not dir #2
+					  Add filters to be able to scan directories selectively; ex. dir #1, dir #3 but not dir #2
  */
 
 // Declare node.js module dependencies from API
-var walk = require('walk'), 
-	fs = require('fs'), 
-	filter;
+var wrench = require('wrench');
 
-// Declare variables	
-var files = []; 
-var i = 0;
-var foldername = 'views';
-var filter = {
-	followLinks: false
+var rootfolder = './node_modules';
+var filteredfiles = [];
+var files = [];
+var fileextension = '.html';
+
+function directoryScan(rootdirectory){
+	files = wrench.readdirSyncRecursive(rootdirectory)
+	for (var i = 0; i<files.length; i++)
+	{
+		if (files[i].indexOf(fileextension) != -1)
+		{
+			filteredfiles.push(files[i]);
+		}
+	}
+	return filteredfiles;
 };
-var walker = walk.walkSync(foldername, filter);	
 
-// Try to avoid using 'walk'
-/* module.exports = {
-	scanDir: function () {
-		var text =  walker.on('file', function(root, stat, next) {
-			files.push(stat.name);
-			filteredFiles.push(stat.name);
-			console.log(files[i]);
-			next();
-			i++;
-		});
-		return text;
-	}
-}; */
+console.log(directoryScan(rootfolder));
 
-// Define module to be exported as a function(s)
-module.exports = {
-	scanDir: function () {
-		// Call synchronous fileSystem function readdir on dir name
-		var text = fs.readdirSync('./views', 'utf8');
-		return text;
-	}
-}
